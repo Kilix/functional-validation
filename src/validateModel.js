0a031d1e4b3ef99db41d0a9c258287dec2f772d7
Shegun Mensah-Beauclair <smensahbeauclair@extia.fr>
2020-09-23 16:47:25 +0200

    fix/refacto: allow memoization of files in validateModel with upgrade of ramda

--- src/validateModel.js
+++ src/validateModel.js
@@ -1,6 +1,6 @@
 // @flow
 import pipe from 'ramda/src/pipe';
-import memoize from 'ramda/src/memoize';
+import memoizeWith from 'ramda/src/memoizeWith';
 import flattenR from 'ramda/src/flatten';
 import mapR from 'ramda/src/map';
 import filterR from 'ramda/src/filter';
@@ -25,13 +25,24 @@ export type ModelValidatorT<T, P: $ReadOnlyArray<any> = []> = (
 ) => $ReadOnlyArray<ErrorT>;
 export type ValidationsT<T, P: $ReadOnlyArray<any>> = $ReadOnlyArray<ValidationT<T, P>>;
 
+function isEqual(elem: Object) {
+    const newElem = {
+        ...elem,
+        ...(elem.files ? { files: elem.files.map(({ name, size }) => ({ name, size })) } : {}),
+        ...(elem.file ? { file: { name: elem.file.name, size: elem.file.size } } : {}),
+    };
+
+    return JSON.stringify(newElem);
+}
 function validateModel<T, P: $ReadOnlyArray<any>>(
     validations: ValidationsT<T, P>,
 ): ModelValidatorT<T, P> {
-    return memoize((model: T, ...params: P): $ReadOnlyArray<ErrorT> =>
-        pipe(map(validation => validation(model, ...params)), flatten, filter(Boolean))(
-            validations,
-        ),
+    return memoizeWith(isEqual, (model: T, ...params: P): $ReadOnlyArray<ErrorT> =>
+        pipe(
+            map(validation => validation(model, ...params)),
+            flatten,
+            filter(Boolean),
+        )(validations),
     );
 }
 
